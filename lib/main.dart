52aad5b19c8111071dae9e9d6ac38a8e159b8c15
CaptainBiswa69 <76955940+CaptainBiswa69@users.noreply.github.com>
2023-08-05 19:55:52 +0530

    Firebase Setup IOs

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ship_easy/views/Splash%20Screen/splash_screen.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
